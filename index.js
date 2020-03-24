f44bfd353ee984c840ad10fdda032bf17fc73991
felixvanriessen <felixvanriessengurung@gmail.com>
2020-03-24 13:03:59 +0100

    nothing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const omdbApiKey = "b4781137"
 const path = require('path')
+const mongoose = require('mongoose')
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,6 +14,14 @@ const hbs = require('hbs')
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+mongoose.connect('mongodb+srv://felix:Three534135@ihp2-zbze0.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,  
+    useUnifiedTopology: true 
+})
+.then((x)=>console.log('connected to database'))
+.catch(err=>console.log(err))
 
 
 app.use('/', require('./routes/indexroute'))
