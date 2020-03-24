600f1b18e0daab377e6c643da898ab9f8aa9432f
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-03-24 18:12:37 +0100

    done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const omdbApiKey = "b4781137"
 const path = require('path')
 const mongoose = require('mongoose')
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,7 +15,6 @@ const hbs = require('hbs')
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 mongoose.connect('mongodb+srv://piepongwong:12345felix@ihp2-zbze0.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,  
@@ -27,6 +27,15 @@ mongoose.connect('mongodb+srv://piepongwong:12345felix@ihp2-zbze0.mongodb.net/te
 app.use('/', require('./routes/indexroute'))
 app.use('/', require('./routes/auth'))
 app.use('/', require('./routes/main'))
+
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: {maxAge: 60000},
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 24 * 60 * 60
+    })
+}));
 
 app.listen(3000, () =>{
     console.log('listening on', 3000)
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+
+// router.use((req,res,next) => {
+//     if(req.session.currentUser) {
+//         next()
+//     } else {
+//         res.redirect("/login")
+//     }
+// })
+
 router.get("/cinema", (req,res) => {
     res.render("cinema")
 })
