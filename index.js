e21da9768de19d24e7c33e6eb21416b745e19f4d
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-03-24 09:56:26 +0100

    evening send

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const omdbApiKey = "b4781137"
+const path = require('path')
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -8,10 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const hbs = require('hbs')
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/indexroute'))
 app.use('/', require('./routes/auth'))
+app.use('/', require('./routes/main'))
 
-app.listen(3000, ()=>{
+app.listen(3000, () =>{
     console.log('listening on', 3000)
 })
