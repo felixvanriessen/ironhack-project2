5f779bd87c5d1bbb54e88d2338dd2c1a68f166ee
felixvanriessen <felixvanriessengurung@gmail.com>
2020-03-24 09:58:04 +0100

    d2

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const omdbApiKey = "b4781137"
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(express.static('public'))
+
 const hbs = require('hbs')
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
@@ -12,7 +14,6 @@ app.set('view engine', 'hbs')
 
 app.use('/', require('./routes/indexroute'))
 app.use('/', require('./routes/auth'))
-
 
 app.listen(3000, ()=>{
     console.log('listening on', 3000)
