afe8944ab95498b98aebd6f788408ac2c59241fd
felixvanriessen <felixvanriessengurung@gmail.com>
2020-03-25 10:40:27 +0100

    crypt fix

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const User = require('../models/usermodel')
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+const session = require('')
 
 router.get('/signup', (req,res)=>{
     res.render('signup')
@@ -28,7 +29,7 @@ router.post('/signup', (req,res) => {
         password: hashPass
     })
     .then(()=> {
-        res.redirect("/");
+        res.redirect("/login");
     })
     .catch(error => {
         console.log(error)
@@ -43,39 +44,47 @@ router.post("/login", (req,res) => {
     const theUsername = req.body.username;
     const thePassword = req.body.password;
 
-    if(theUsername === "" || thePassword === "") {
-        res.render("login",{errorMessage: "Please enter both username and password to sign up"})
-        return;
-    }
+    // if(theUsername === "" || thePassword === "") {
+    //     res.render("login",{errorMessage: "Please enter both username and password to sign up"})
+    //     return;
+    // }
 
     User
     .findOne({"username": theUsername})
     .then(user => {
         if(!user) {
             res.render("login", {errorMessage: "The username doesn't exist"})
-            return
         }
-
-        if(bcrypt.compareSync(thePassword,user.password)) {
-            req.session.currentUser = user;
-            res.redirect("/") 
-        } else {
-            res.render("login", {errorMessage: "Incorrect password"});
+        else {
+            bcrypt.compare(thePassword, user.password, function(err, result) {
+                if (!result) {
+                    res.send('incorrect')
+                }
+                else {
+                    req.session.currentUser = user
+                    res.redirect('/search')
+                }
+            })
         }
     })
-    .catch(error => {
-        next(error);
+        // if(bcrypt.compareSync(thePassword,user.password)) {
+        //     req.session.currentUser = user;
+        //     res.redirect("/") 
+        // } else {
+        //     res.render("login", {errorMessage: "Incorrect password"});
+        // }
+    .catch(error => {console.log(error)
     })
-})
 
-router.get("/logout", (req,res,next) => {
-    req.session.destroy((err) => {
-        res.redirect("/login")
-    });
-});
 
-router.get('/settings', (req,res)=>{
-    res.render('settings')
-})
+// router.get("/logout", (req,res,next) => {
+//     req.session.destroy((err) => {
+//         res.redirect("/login")
+//     });
+// });
+
+// router.get('/settings', (req,res)=>{
+//     res.render('settings')
+// })
 
 module.exports = router
